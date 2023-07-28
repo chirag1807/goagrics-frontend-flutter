7300a661165972cdff52c19b093f21b972c7eafd
Ankit Bose <105658209+prog-ankit@users.noreply.github.com>
2023-07-28 14:56:28 +0530

    Add files via upload

---
+++ lib/auth_database/AuthServices.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class AuthServices {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<String> verifyPhone(String number, BuildContext context) async {
+    String verifyId = "";
+
+    try {
+      auth.verifyPhoneNumber(
+        phoneNumber: number,
+        verificationCompleted: (_) {},
+        verificationFailed: (e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+              e.toString(),
+            ),
+          ));
+          verifyId = "error " + e.toString();
+        },
+        codeSent: ((verificationId, token) {
+          print("Id here: " + token.toString());
+          verifyId = verificationId;
+        }),
+        codeAutoRetrievalTimeout: (e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+              e.toString(),
+            ),
+          ));
+
+          verifyId = "error " + e.toString();
+        },
+      );
+    } catch (e) {
+      verifyId = "error " + e.toString();
+    }
+
+    return verifyId;
+  }
+}
